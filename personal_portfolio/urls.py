e505c6bd12278f8164176bd62b72d74ea8d7b542
Amreen Shaik <amreenshaik.0207@gmail.com>
2020-07-15 15:16:08 -0600

    Save my changes

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,6 +22,7 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('contact/', views.contact_form, name='contact'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
