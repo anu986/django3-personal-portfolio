20fa6ba7c594667e9a1281e1702c67a38b563ff2
anu986 <68044114+anu986@users.noreply.github.com>
2020-07-15 21:48:03 -0600

    Update urls.py

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,7 +22,6 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('contact/', views.contact_form, name='contact'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
