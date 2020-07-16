46fe737cc57384892c4c8bb7fed5d9e5304f9949
Amreen Shaik <amreenshaik.0207@gmail.com>
2020-07-15 21:49:28 -0600

    Save all my changes

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,9 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = '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'
-SENDGRID_SANDBOX_MODE_IN_DEBUG=False
 
 try:
     from .local_settings import *
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,26 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
-
-from .forms import ContactForm
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
 
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects':projects})
-
-def contact_form(request):
-    form = ContactForm()
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = f'Message from {form.cleaned_data.get("name")}'
-            message = form.cleaned_data.get("message"),
-            sender = form.cleaned_data.get("email"),
-            recipients = ['amreen9797@gmail.com']
-            try:
-                send_mail(subject, message, sender, recipients, fail_silently=True)
-            except BadHeaderError:
-                return HttpResponse('Invalid header found')
-            return HttpResponse('Success...Your email has been sent')
-    return render(request, 'portfolio/contact.html', {'form': form})
